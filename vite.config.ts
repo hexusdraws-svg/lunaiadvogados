60a328736a75c351e97c62e1c99736f65b98a0f3
hexusdraws-svg <hexusdraws@gmail.com>
2026-08-19 20:53:21 +0300

    g2g

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,10 @@ export default defineConfig({
   },
   nitro: {
     preset: "vercel",
+    output: {
+      dir: ".vercel/output",
+      serverDir: ".vercel/output/functions",
+      publicDir: ".vercel/output/static",
+    },
   },
 });
